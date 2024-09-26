98ede259cea9d9bf9d7306331239e091e5b4f622
stellar-demesne <54080611+stellar-demesne@users.noreply.github.com>
2024-09-25 21:39:29 -0700

    Basic construction, initial attempt at info

---
+++ VoidMapClarifier.js
@@ -0,0 +1,94 @@
+initialiseVoidMapClarifier();
+
+function initialiseVoidMapClarifier() {
+    if (document.getElementById('VoidMapClarifier') === null) {
+        const containerVoidMapInfo = document.createElement('DIV');
+  
+        let standard_colours = ' color: rgb(0,0,0); background-color: rgb(255,255,255);';
+        let darkmode_colours = ' color: rgb(0,0,0); background-color: rgb(93,93,93);';
+    
+        let chosen_colours = standard_colours;
+        if (game.options.menu.darkTheme.enabled == 2) {
+            chosen_colours = darkmode_colours;
+        }
+        containerVoidMapInfo.setAttribute('style', 'display: block; position: absolute; top: 0; right: 0; width: 30%; font-size: 0.7em; text-align: center;' + chosen_colours);
+        const textareaVoidmapInfo = document.createElement('SPAN');
+        containerVoidMapInfo.setAttribute('onmouseover', VMC_populateVoidMapTooltip());
+        containerVoidMapInfo.setAttribute('onmouseout', 'tooltip("hide")');
+        textareaVoidmapInfo.id = 'VoidMapClarifier';
+        containerVoidMapInfo.appendChild(textareaVoidmapInfo);
+        let target_area = document.getElementById('science');
+        target_area.insertBefore(containerVoidMapInfo, target_area.children[0]);
+    }
+    populateVoidMapClarifierInfo();
+    setInterval( function () {
+        populateVoidMapClarifierInfo();
+    }, 1000);
+}
+
+function VMC_getCurrentVMDCeffect() {
+    let shieldbonus = (1 - (getHeirloomBonus("Shield", "voidMaps") / 100))
+    let extraV = 0;
+	if (game.challenges.Nurture.boostsActive() && game.challenges.Nurture.getLevel >= 4) {
+        extraV = 0.2;
+    }
+    let goldenbonus = (1 - (game.goldenUpgrades.Void.currentBonus + extraV))
+    return shieldbonus * goldenbonus
+}
+
+function VMC_getsocalledZ() {
+    let max = getVoidMaxLevel();
+    let lastPortal = getLastPortal();
+    if (lastPortal != -1) && (max - lastPortal < 25) {
+        max = lastPortal;
+    }
+    if (max > 200) {
+        max = 200;
+    }
+    let min = (max > 80) ? (1000 + ((max - 80) * 13)) : 1000;
+
+    return min
+}
+
+function VMC_getCurrentVMDCodds() {
+    let netBonus = VMC_getCurrentVMDCeffect();
+    let naturalCooldownCellCount = VMC_getsocalledZ();
+    let cooldownCellCount = netBonus * naturalCooldownCellCount;
+
+    let timeSinceVoid = game.global.lastVoidMap;
+    if (timeSinceVoid < cooldownCellCount) {
+        return 'cells left: ' + (cooldownCellCount - timeSinceVoid);
+    }
+    let cellsPastCooldown = timeSinceVoid - cooldownCellCount;
+    let currentChance = Math.floor(cellsPastCooldown / 10) / 50000;
+    return '' + currentChance + '%-per-cell';
+}
+
+function VMC_makeStringForDisplay() {
+	if (game.global.totalPortals < 1) || (game.global.universe == 2 && game.global.totalRadPortals < 1) {
+        return 'N/A';
+    }
+    
+    const voidmapstring = VMC_getCurrentVMDCeffect() + "<br\>" + VMC_getCurrentVMDCodds();
+    return voidmapstring
+}
+
+function VMC_populateVoidMapTooltip() {
+    if (usingRealTimeOffline == true) {
+      return '';
+    }
+    let tooltipstring = '';
+    tooltipstring = "you've gotten " + getCurrentTotalVoids() + " voids total!";
+    return tooltipstring
+}
+
+function populateVoidMapClarifierInfo() {
+    if (usingRealTimeOffline == true) {
+      return '';
+    }
+
+    const target_element = document.getElementById('VoidMapClarifier');
+    const the_information = VMC_makeStringForDisplay();
+    target_element.innerHTML = the_information;
+    target_element.parentNode.setAttribute('onmouseover', VMC_populateVoidMapTooltip());
+}

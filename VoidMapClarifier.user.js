51d257e97ace2c5c21a19cfa2535f0a725e12bf1
stellar-demesne <54080611+stellar-demesne@users.noreply.github.com>
2024-09-25 21:51:33 -0700

    Create VoidMapClarifier.user.js

---
+++ VoidMapClarifier.user.js
@@ -0,0 +1,16 @@
+// ==UserScript==
+// @name         Runetrinket-Counter
+// @namespace    https://github.com/stellar-demesne/Trimps-VoidMapClarifier
+// @version      1.0
+// @updateURL    https://github.com/stellar-demesne/Trimps-VoidMapClarifier/VoidMapClarifier.user.js
+// @description  Runetrinket Counter for Trimps
+// @author       StellarDemesne
+// @include      *trimps.github.io*
+// @include      *kongregate.com/games/GreenSatellite/trimps
+// @grant        none
+// ==/UserScript==
+var script = document.createElement('script');
+script.id = 'VoidMap-Clarifier';
+script.src = 'https://stellar-demesne.github.io/Trimps-VoidMapClarifier/VoidMapClarifier.js';
+script.setAttribute('crossorigin', "anonymous");
+document.head.appendChild(script);

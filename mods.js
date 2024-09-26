c59b7dcd59607d0c8380f6160487334d831e60be
stellar-demesne <54080611+stellar-demesne@users.noreply.github.com>
2024-09-25 21:50:28 -0700

    Create mods.js

---
+++ mods.js
@@ -0,0 +1,5 @@
+var script = document.createElement('script');
+script.id = 'VoidMap-Clarifier';
+script.src = 'https://stellar-demesne.github.io/Trimps-VoidMapClarifier/VoidMapClarifier.js';
+script.setAttribute('crossorigin', "anonymous");
+document.head.appendChild(script);

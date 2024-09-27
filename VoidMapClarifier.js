2d947c6885db7dc53ed51c52ca39c56549d5b68d
stellar-demesne <54080611+stellar-demesne@users.noreply.github.com>
2024-09-26 17:34:00 -0700

    fix maths error on the permaBonus counter

--- VoidMapClarifier.js
+++ VoidMapClarifier.js
@@ -187,8 +187,8 @@ function VMC_stringifyCurrentBoneBonusTimer() {
     if (current_number_owned == 0) {
         return ''
     }
-    let drops_until_next_double = Math.floor((100 - current_tracker_moment) / current_number_owned);
-    if (drops_until_next_double == 0) {
+    let drops_until_next_double = Math.floor((99 - current_tracker_moment) / current_number_owned);
+    if (drops_until_next_double <= 0) {
         return 'Your next void map drop will be duplicated!'
     }
     let returntext = 'Your next duplicated void map drop is <b>' + drops_until_next_double + '</b> drop';
